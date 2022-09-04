9873d244ab5d3722520516e6a7882cddd41cd6ea
Akram Ahamed <akram.ahammad@gmail.com>
2022-09-04 08:59:27 +0530

    Added auth interceptor for adding jwt token

---
+++ src/app/interceptors/auth-interceptor.service.ts
@@ -0,0 +1,21 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor{
+
+  constructor(private auth:AuthService) { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    console.log('Inside auth interceptor')
+    const token=this.auth.getToken()
+    const authReq=req.clone({
+      headers:req.headers.set('Authorization',`Bearer ${token}`)
+    })
+
+    return next.handle(authReq);
+  }
+
+}
